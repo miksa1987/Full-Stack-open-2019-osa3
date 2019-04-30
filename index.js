eaffd6599f28ee9a79611b34601a4d2e7718bec6
x <mika87@gmail.com>
2019-04-30 17:23:38 +0300

    3.1 tehty

---
+++ index.js
@@ -0,0 +1,34 @@
+// Here goes
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Martti Tienari",
+    number: "040-123456",
+    id: 2
+  },
+  {
+    name: "Arto Järvinen",
+    number: "040-123456",
+    id: 3
+  },
+  {
+    name: "Lea Kutvonen",
+    number: "040-123456",
+    id: 4
+} ]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT=3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
