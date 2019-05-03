8a5edd3d049f8b3b007cdc337feb2a322416b5be
x <mika87@gmail.com>
2019-05-03 09:40:18 +0300

    3.19 tehty

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,6 +1,11 @@
+
+
 const errorHandler = (error, response, request, next) => {
     if (error.name === 'CastError' && error.kind == 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: 'Name must be unique!' })
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -1,16 +1,20 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGO_URI
+
+mongoose.set('useCreateIndex', true)
 
 mongoose.connect(url, { useNewUrlParser: true })
     .then(result => { console.log('connected to MongoDB')})
     .catch((error) => { console.log('error connecting to MongoDB: ' + error.message)})
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: { type: String, required: true, unique: true },
+    number: { type: String, required: true }
 })
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
